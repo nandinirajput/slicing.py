4c8122d33d14b9f96bb32b5eba4edaae4434a8e9
RajputNandini <109655286+nandinirajput@users.noreply.github.com>
2022-07-29 12:31:49 +0530

    Add files via upload

---
+++ Slicing.py
@@ -0,0 +1,57 @@
+# x='nandini'
+# print(x[0])
+# print(x[2])
+# print(x[4])
+# print(x[3])
+# print(x[5])
+# print(x[1])
+
+# y='python'
+# print(y[2:])
+# print(y[2:6])
+# print(y[:5])
+# print(y[2:3])
+# print(y[4:1])
+
+# print(y[-4])
+# print(y[-5])
+# print(y[-3])
+# print(y[-2])
+# print(y[-1])
+# print(y[-6])
+# print(y[-7])
+
+# print(y[-4]+y[-2])
+# print(y[0]+y[-4])
+
+"""program to find the sum of the first and last digit"""
+# c='542182012245532'
+# print(type(c))
+# print(c[2])
+# print(c[-4])
+# print(c[2+c[-6]])
+# print(int(c{2})+int(c{-2}))
+# print(int(c{-4})+int(c{-6}))
+
+# f='python'
+# print(f{2:7})
+# print(f{2:-1})
+# print(f{1:-2})
+# print(f{-4:0})
+# print(f{-1:1})
+# print(f{-4:0})
+
+"""reverse of a string"""
+# n=water
+# print(n{::-1})
+
+# k=nandini
+# print(k[::-1])
+# print(k[::-2])
+# print(k[::-3])
+# print(k[::-4])
+# print(k[::-5])
+# print(k[::-6])
+# print(k[::-7])
+# print(k[::])
+
